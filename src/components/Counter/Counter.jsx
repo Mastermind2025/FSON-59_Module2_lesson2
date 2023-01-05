43b54e2588ef2816b8be871992b40abbf761718c
Mastermind2025 <85400723+Mastermind2025@users.noreply.github.com>
2023-01-06 00:01:28 +0200

    fix

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 // document.addEventListener('clicl', (event) => {})
 // let count = 0;
 
